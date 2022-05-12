3c27dddbd666f932a431024d85434fd39b02990a
jongyoon-park <cleanbook@naver.com>
2022-05-12 14:33:58 +0900

    made CRA

---
+++ cat-jjal-maker-cra/src/App.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import Title from './components/Title';
+import './App.css';
+
+const jsonLocalStorage = {
+  setItem: (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  getItem: (key) => {
+    return JSON.parse(localStorage.getItem(key));
+  },
+};
+
+const fetchCat = async (text) => {
+  const OPEN_API_DOMAIN = "https://cataas.com";
+  const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
+  const responseJson = await response.json();
+  return `${OPEN_API_DOMAIN}/${responseJson.url}`;
+};
+
+const Form = ({ updateMainCat }) => {
+  const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text); //한글 입력 여부 판별
+  const [value, setValue] = React.useState('');
+  const [errorMessage, setErrorMessage] = React.useState('');
+
+  function handleInputChange(e) {
+    const userValue = e.target.value;
+    console.log(includesHangul(userValue));
+    setErrorMessage(''); //한글이 입력되지 않았을 때 에러 메시지를 초기화
+    if (includesHangul(userValue)) {
+      setErrorMessage("한글은 입력할 수 없습니다.")
+    }
+    setValue(e.target.value.toUpperCase());
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    if (errorMessage === '한글은 입력할 수 없습니다.') {
+      return;
+    }
+    setErrorMessage('');
+    if (value === '') {
+      setErrorMessage("값을 입력해주세요.");
+      return; //에러메시지 출력 후 함수 종료 시키기
+    }
+    updateMainCat(value);
+  }
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        name="name" p
+        placeholder="영어 대사를 입력해주세요"
+        value={value}
+        onChange={handleInputChange}
+      />
+      <button type="submit">생성</button>
+      <p style={{ color: "red" }}>{errorMessage}</p>
+    </form>
+  )
+}
+
+function CatItem(props) {
+  return (
+    <li>
+      <img src={props.img} style={{ width: "150px", border: "1px solid red" }} />
+    </li>
+  );
+}
+
+function Favorites({ favorites }) {
+  if (favorites.length === 0) {
+    return <div>사진 위 하트를 눌러 고양이 사진을 저장해봐요!</div>
+  }
+  return (
+    <ul className="favorites">
+      {favorites.map(cat => <CatItem img={cat} key={cat} />)}
+    </ul>
+  );
+}
+
+const MainCard = ({ img, onHeartClick, alreadyFavorite }) => {
+  const heartIcon = alreadyFavorite ? "💖" : "🤍"; //삼항연산자
+  return (
+    <div className="main-card">
+      <img src={img} alt="고양이" width="400" />
+      <button onClick={onHeartClick}>{heartIcon}</button>
+    </div>
+  );
+}
+
+const App = () => {
+  const CAT1 = "https://cataas.com/cat/60b73094e04e18001194a309/says/react";
+  const CAT2 = "https://cataas.com//cat/5e9970351b7a400011744233/says/inflearn";
+  const CAT3 = "https://cataas.com/cat/595f280b557291a9750ebf65/says/JavaScript";
+  const [counter, setCounter] = React.useState(() => {
+    return jsonLocalStorage.getItem('counter')
+  });
+  const [mainCat, setMainCat] = React.useState(CAT1);
+  const [favorites, setFavorites] = React.useState(() => {
+    return jsonLocalStorage.getItem('favorites') || []
+  });
+
+  const alreadyFavorite = favorites.includes(mainCat);
+  console.log("카운터", counter);
+
+  async function setInitialCat() {
+    const newCat = await fetchCat('first cat');
+    console.log(newCat);
+    setMainCat(newCat);
+  }
+
+  React.useEffect(() => {
+    setInitialCat();
+  }, [])
+
+  // setInitialCat();
+
+  async function updateMainCat(value) {
+    const newCat = await fetchCat(value);
+    setMainCat(newCat);
+    setCounter((prev) => {
+      const nextCounter = prev + 1;
+      jsonLocalStorage.setItem("counter", nextCounter);
+      return prev + 1;
+    })
+  }
+
+  function handleHeartClick() {
+    const nextFavorites = [...favorites, mainCat];
+    setFavorites(nextFavorites);
+    jsonLocalStorage.setItem('favorites', nextFavorites);
+  }
+
+  const counterTitle = counter === null ? "" : counter + "번째 ";
+
+  return (
+    <div>
+      <Title>{counterTitle}고양이 가라사대</Title>
+      <Form updateMainCat={updateMainCat} />
+      <MainCard img={mainCat} onHeartClick={handleHeartClick} alreadyFavorite={alreadyFavorite} />
+      <Favorites favorites={favorites} />
+    </div>
+  );
+}
+
+export default App;
